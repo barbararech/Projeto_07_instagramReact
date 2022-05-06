19bb40e524261320912fbf28657e52e23745512b
barbararech <rech.brb@gmail.com>
2022-05-06 12:16:25 -0300

    Props - Feed

--- src/Feed.js
+++ src/Feed.js
@@ -1,87 +1,73 @@
-export default function Feed(){
-    return (
-        <div className="feed">
-        {/* <!-- Post 1 --> */}
-        <div className="post">
-            <div className="head-post">
-                <img src="/img/meowed1.png" alt="post"/>
-                <div className="post-titulo-usuario">
-                    <p>meowed</p>
-                </div>
-                <div className="etc">
-                    <ion-icon name="ellipsis-horizontal"></ion-icon>
-                </div>
-            </div>
-            <div className="img-post">
-                <img src="/img/gato-telefone 1.png" alt="post"/>
-            </div>
-            <div className="foot-post">
-                <div className="container-acoes">
-                    <div className="acoes">
-                        <div className="icon-2"><ion-icon name="heart-outline"></ion-icon></div>
-                        <div className="icon-2"><ion-icon name="chatbubble-outline"></ion-icon></div>
-                        <div className="icon-2"><ion-icon name="paper-plane-outline"></ion-icon></div>
-                    </div>
-                    <div className="container-save">
-                        <div className="icon-2"><ion-icon name="bookmark-outline"></ion-icon></div>
-                    </div>
-                </div>
-            </div>
-            <div className="post-likes">
-                <img src="/img/respondeai 1.png" alt="post"/>
-                <p> Curtido por <strong>respondeai</strong> e <strong>outras 101.523 pessoas</strong> </p>
-            </div>
-        </div>
-        {/* <!-- Post 2 --> */}
-        <div className="post">
-            <div className="head-post">
-                <img src="/img/barked 1.png" alt="post"/>
-                <div className="post-titulo-usuario">
-                    <p>barked</p>
-                </div>
-                <div className="etc">
-                    <ion-icon name="ellipsis-horizontal"></ion-icon>
-                </div>
-            </div>
-            <div className="img-post">
-                <video width="100%"  autoPlay loop muted>
-                    <source src="/img/video.mp4" type="video/mp4"/>
-                    <source src="/img/video.ogv" type="video/ogg"/>
-                    Your browser does not support the video tag.
-                </video> 
-            </div>
-            <div className="foot-post">
-                <div className="container-acoes">
-                    <div className="acoes">
-                        <div className="icon-2"><ion-icon name="heart-outline"></ion-icon></div>
-                        <div className="icon-2"><ion-icon name="chatbubble-outline"></ion-icon></div>
-                        <div className="icon-2"><ion-icon name="paper-plane-outline"></ion-icon></div>
-                    </div>
-                    <div className="container-save">
-                        <div className="icon-2"><ion-icon name="bookmark-outline"></ion-icon></div>
-                    </div>
-                </div>
-            </div>
-            <div className="post-likes">
-                <img src="/img/adorableanimals 1.png" alt="post"/>
-                <p> Curtido por <strong>adorable_animals</strong> e <strong>outras 99.159 pessoas</strong> </p>
-            </div>
-        </div>
-        {/* <!-- Post 3 --> */}
-        <div className="post">
-            <div className="head-post">
-                <img src="/img/9gag 1.png" alt="post"/>
-                <div className="post-titulo-usuario">
-                    <p>9gag</p>
-                </div>
-                <div className="etc">
-                    <ion-icon name="ellipsis-horizontal"></ion-icon>
-                </div>
-            </div>
-            <div className="img-post">
-                <img src="/img/gato-telefone 3.png" alt="post"/>
-            </div>
-        </div>
-    </div>  
+function Icons(props){
+    return(
+        <div className={props.className}><ion-icon name={props.name}></ion-icon></div>
     )
 }
+
+function Posts(props) {
+    return (
+        <div className="post">
+            <div className="head-post">
+                <img src={props.imgUser} alt="post"/>
+                <div className="post-titulo-usuario">
+                    <p>{props.title}</p>
+                </div>
+                <div className="etc">
+                    <Icons name="ellipsis-horizontal" />
+                </div>
+            </div>
+            <div className="img-post">
+                <img src={props.imgPost} alt="post"/>
+            </div>
+            <div className="foot-post">
+                <div className="container-acoes">
+                    <div className="acoes">
+                        <Icons name="heart-outline" className="icon-2" />
+                        <Icons name="chatbubble-outline" className="icon-2" />
+                        <Icons name="paper-plane-outline" className="icon-2" />
+                    </div>
+                    <div className="container-save">
+                        <Icons name="bookmark-outline" className="icon-2" />
+                    </div>
+                </div>
+            </div>
+            <div className="post-likes">
+                <img src={props.imgLike} alt="post"/>
+                <p> Curtido por <strong>{props.userLiked}</strong> e <strong>outras {props.numberLiked} pessoas</strong> </p>
+            </div>
+        </div>
+    );
+}
+
+export default function Feed() {
+    // Logic
+    const items = [
+      {
+        title: "meowed",
+        imgUser: "/img/meowed1.png",
+        imgPost: "/img/gato-telefone 1.png",
+        imgLike:"/img/respondeai 1.png",
+        liked:"respondeai",
+        numberLiked:"101.523"
+      },
+      {
+        title: "barked",
+        imgUser: "/img/barked 1.png",
+        imgPost: "/img/dog 1.png",
+        imgLike:"/img/adorableanimals 1.png",
+        liked:"adorable_animals",
+        numberLiked:"99.159"
+      }
+    ];
+  
+    const posts = items.map((item) => (
+      <Posts title={item.title} imgUser={item.imgUser} imgPost={item.imgPost} imgLike={item.imgLike} userLiked={item.liked} numberLiked={item.numberLiked} />
+    ));
+
+    // UI
+    return (
+        <div className="feed">
+            {posts}
+        </div>  
+    );
+  }
